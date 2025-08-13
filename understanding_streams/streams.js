8db68e8b11bb49f50892b181965b3bf8ab886367
Resistance21 <brendan_hales@hotmail.com>
2025-08-13 16:13:46 +0800

    first commit

---
+++ understanding_streams/streams.js
@@ -0,0 +1,33 @@
+import fs from "node:fs/promises";
+const streamReader = async () => {
+    try {
+        console.time("copy");
+        const originalFile = await fs.open("./dummy_data.txt", "r");
+        const copiedFile = await fs.open("./Copied-File.txt", "w");
+        let dataRead = -1;
+        while (dataRead !== 0) {
+            const originalFileRead = await originalFile.read();
+            dataRead = originalFileRead.bytesRead;
+            if (dataRead === 0)
+                break;
+            if (dataRead !== 16384) {
+                const endBuffer = Buffer.alloc(dataRead);
+                originalFileRead.buffer.copy(endBuffer, 0, 0, dataRead);
+                console.log(`smaller, ${dataRead}`);
+                copiedFile.write(endBuffer);
+            }
+            else {
+                copiedFile.write(originalFileRead.buffer);
+            }
+        }
+        originalFile.close();
+        copiedFile.close();
+    }
+    catch (error) {
+        console.log(`Error message ${error}`);
+    }
+    console.log("done");
+    console.timeEnd("copy");
+};
+streamReader();
+//# sourceMappingURL=streams.js.map
